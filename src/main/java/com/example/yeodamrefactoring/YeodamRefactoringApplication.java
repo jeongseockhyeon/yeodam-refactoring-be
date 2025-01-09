68a94068c878e359d7a302308c3338fcdb45bff0
jeongseokhyeon <jeong9235@gmail.com>
2025-01-09 17:27:23 +0900

    초기 설정

---
+++ src/main/java/com/example/yeodamrefactoring/YeodamRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.yeodamrefactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeodamRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeodamRefactoringApplication.class, args);
+    }
+
+}

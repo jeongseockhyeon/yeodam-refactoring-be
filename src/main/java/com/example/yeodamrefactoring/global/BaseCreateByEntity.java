83fd0404f4a6f719dff78a772b31982b143e136a
jeongseokhyeon <jeong9235@gmail.com>
2025-01-09 17:43:47 +0900

    feat: 베이스 엔티티 추가

---
+++ src/main/java/com/example/yeodamrefactoring/global/BaseCreateByEntity.java
@@ -0,0 +1,22 @@
+package com.example.yeodamrefactoring.global;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseCreateByEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
+}

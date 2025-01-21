3c2e67f9ef5b4b68fbd824ab9f0aae3695e1ea51
jeongseokhyeon <jeong9235@gmail.com>
2025-01-21 11:01:58 +0900

    #2 Feat: Cors 설정 추가

--- src/main/java/com/example/yeodamrefactoring/global/config/SecurityConfig.java
+++ src/main/java/com/example/yeodamrefactoring/global/config/SecurityConfig.java
@@ -30,13 +30,14 @@ public class SecurityConfig {
     private final AccessDeniedHandler accessDeniedHandler;
     private final AuthenticationConfiguration authenticationConfiguration;
 
-
     @Bean
     public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors
+                        .configurationSource(CorsConfig.corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/",
                                 "/api/v1/users/join",
